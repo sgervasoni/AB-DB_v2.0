c852dcee97cee6c9cd096e854f36af9849418512
Silvia Gervasoni <122807057+sgervasoni@users.noreply.github.com>
2026-09-15 11:08:25 +0200

    Update 3-extract_average.sh

--- MD/3-extract_average.sh
+++ MD/3-extract_average.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-drug=$(ls *_solv.parm7 | awk 'BEGIN{FS="_solv"}{print $1}')
+# File names are consistent with those presented in AB-DB v2.0
+# Please insert the compound name
+drug=INSERT_COMPOUND_NAME
+
 TOP=${drug}_solv.parm7
 TRAJ=md_NPT.nc
 IMAGED=md_imaged_skip1.nc
 IMAGED_SOLUTE=${drug}_imaged_skip1.nc
 
-###extract the average structure of the ligand from the imaged trajectory
+# Extract the average structure of the ligand from the imaged trajectory
 cat > cpptraj_extract.in <<EOF
 parm ${drug}.parm7 [top]
 trajin ${IMAGED_SOLUTE}
