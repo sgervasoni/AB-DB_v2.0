9489488c5351ee3dc0a1c96f901465ef5754ef89
Silvia Gervasoni <122807057+sgervasoni@users.noreply.github.com>
2026-09-15 11:16:04 +0200

    Update and rename 9-plumed-gromacs.sh to 8-plumed-gromacs.sh

--- MD/8-plumed-gromacs.sh
+++ MD/8-plumed-gromacs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-dir="/data/biodb/SCRIPTS/bin"
+# File names are consistent with those presented in AB-DB v2.0
+# Please insert the compound name
+drug=INSERT_COMPOUND_NAME
 
-drug=$(ls *_solv.parm7 | awk 'BEGIN{FS="_solv"}{print $1}')
 PDB=${drug}.pdb
 DCD=${drug}_imaged_skip1.dcd
 TRR=${drug}_imaged_skip1.trr
 
-##substitution of number of atoms of ligand in plumed.inp file
+# Substitution of the number of atoms of ligand in plumed.inp file
 var1=$(grep MOL -o ${drug}_solv.pdb | wc -l)
 var2=`expr ${var1} - 1`
 sed -i "s/XXXXX/${var2}/g" plumed.inp
@@ -15,14 +16,13 @@ sed -i "s/XXXXX/${var2}/g" plumed.inp
 echo
 echo "RUNNING catdcd on ${lig}"
 echo
-$dir/catdcd -o ${DCD} -otype dcd -netcdf ${drug}_imaged_skip1.nc > output_catdcd
-$dir/catdcd -o ${TRR} -otype trr -netcdf ${drug}_imaged_skip1.nc > output_catdcd2
+catdcd -o ${DCD} -otype dcd -netcdf ${drug}_imaged_skip1.nc > output_catdcd
+catdcd -o ${TRR} -otype trr -netcdf ${drug}_imaged_skip1.nc > output_catdcd2
 echo
 echo "DONE catdcd on ${lig}"
 echo
 echo "DRIVER on ${lig}"
 echo
-#driver -pdb ${PDB} -dcd ${DCD} -plumed plumed.inp -ncv 6 -dt 0.01 > output_driver
 plumed driver --pdb ${drug}.pdb --mf_dcd ${drug}_imaged_skip1.dcd --plumed plumed.inp --timestep 0.01 > output_driver
 echo
 echo "DONE driver on ${lig}"
